373d909ff84913ab2c5e8d4a17bb8ac64bbf65d8
Reginaldo Alves <reginaldoalvesti@outlook.com>
2023-02-20 09:19:44 -0300

    criando arrays D

---
+++ Aulas/Numpy/Criando Arrays em 1D, 2D e 3D..py
@@ -0,0 +1,31 @@
+import numpy as np 
+
+print("************************************************")
+# Array 1D
+d1 = np.array([1,2,3])
+print(d1)
+print(d1.shape)
+print(d1.ndim)
+print(d1.dtype)
+
+print("************************************************")
+
+# Array 2D
+d2 = np.array([[1,2,3], [4,5,6]])
+print(d2)
+print(d2.shape)
+print(d2.ndim)
+print(d2.dtype)
+
+
+print("************************************************")
+
+# Array 3D
+d3 = np.array([[[1,2], [3,4]] , [[5,6], [7,8]]])
+print(d3)
+print(d3.shape)
+print(d3.ndim)
+print(d3.dtype)
+
+print("************************************************")
+
